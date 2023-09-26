60f474ff04886cdef73344eb103a2c051233d0d4
Tristan Pelser <t.pelser@fz-juelich.de>
2023-09-26 12:55:10 +0000

    update the dev container

--- src/utils/Task1_set_region.py
+++ src/utils/Task1_set_region.py
@@ -6,6 +6,7 @@ import json
 
 class Task1(luigi.Task):
     # set all the paths for initialisation and the parameters for running the generator
+    container_name = luigi.Parameter()
     install_dir = luigi.Parameter()
     data_dir = luigi.Parameter()
     output_dir = luigi.Parameter()
@@ -14,49 +15,50 @@ class Task1(luigi.Task):
 
     def _docker_exists(self):
         """
-        Check if a Docker container named 'shapefilegenerator' exists.
+        Check if a Docker container named 'self.container_name' exists.
 
         This function uses the Docker API to look for a container with the specified name. 
         It returns True if the container exists, and False otherwise.
 
         Returns:
-            bool: True if 'shapefilegenerator' container exists, False otherwise.
+            bool: True if container exists, False otherwise.
         """
         client = docker.from_env()
         try:
-            client.containers.get('shapefilegenerator')
+            client.containers.get(self.container_name)
             return True
         except docker.errors.NotFound:
             return False
         
     def _build_and_run_docker(self):
         """
-        Build and run a Docker container named 'shapefilegenerator' from a specified directory.
+        Build and run a Docker container named 'self.container_name' from a specified directory.
 
-        This function uses the Docker API to first build an image with the tag "shapefilegenerator:reflow"
+        This function uses the Docker API to first build an image with the tag "self.container_name:reflow"
         from the directory pointed to by self.install_dir. It then runs a container with the name 
-        "shapefilegenerator" using this image, detached and with tty enabled.
+        "self.container_name" using this image, detached and with tty enabled.
 
         Note:
             Assumes that `self.install_dir` is already set and points to the directory containing the 
             Dockerfile to build the image.
         """
         client = docker.from_env()
-        client.images.build(path=self.install_dir, tag="shapefilegenerator:reflow")
-        client.containers.run("shapefilegenerator:reflow", name="shapefilegenerator", detach=True, tty=True)
+        tag_name = f"{self.container_name}:reflow"
+        client.images.build(path=self.install_dir, tag=tag_name)
+        client.containers.run(tag_name, name=self.container_name, detach=True, tty=True)
 
     def run(self):
         """
         Build (if necessary) and run a Docker container to generate a shapefile.
 
-        This method first checks if a Docker container named 'shapefilegenerator' already exists using
+        This method first checks if a Docker container named 'self.container_name' already exists using
         `_docker_exists()`. If it does not exist, it will build and run the container using 
         `_build_and_run_docker()`.
 
-        Once the container is running, it initializes an instance of the `ShapefileGenerator` class
+        Once the container is running, it initializes an instance of the `self.container_name` class
         inside the container with the necessary parameters. It then imports configuration for 
         shapefile generation from `self.shapefile_params` and runs the `return_shapefile` method from
-        the initialized `ShapefileGenerator` object.
+        the initialized `self.container_name` object.
 
         Note:
             1. Assumes that `self.data_dir`, `self.output_dir`, `self.gadm_version`, and `self.shapefile_params`
@@ -66,7 +68,8 @@ class Task1(luigi.Task):
             self._build_and_run_docker()
 
         client = docker.from_env()
-        container = client.containers.get("shapefilegenerator:reflow")
+        tag_name = f"{self.container_name}:reflow"
+        container = client.containers.get(tag_name)
 
         # Initialise sg class within Docker container
         # This assumes that it is possible to send commands to the container's bash to initialise
