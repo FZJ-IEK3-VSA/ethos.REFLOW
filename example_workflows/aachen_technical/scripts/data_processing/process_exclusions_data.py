20b72b854d3fa9dbc2b2460b8f8694d2a6d79c3e
Tristan Pelser <t.pelser@fz-juelich.de>
2024-04-26 17:25:27 +0200

    Refactor exclusion data processing and update project settings

--- example_workflows/aachen_technical/scripts/data_processing/process_exclusions_data.py
+++ example_workflows/aachen_technical/scripts/data_processing/process_exclusions_data.py
@@ -35,11 +35,12 @@ class ProcessExclusionsData(luigi.Task):
         #### directory management ####
         config_loader = ConfigLoader()
         
-
         project_settings_path = config_loader.get_path("settings", "project_settings")
         project_data_dir = config_loader.get_path("data", "project_data")
         raw_data_dir = config_loader.get_path("data", "exclusion_data", "raw")
         processed_data_dir = config_loader.get_path("data", "exclusion_data", "processed")
+        # create the processed data directory if it does not exist
+        os.makedirs(processed_data_dir, exist_ok=True)
 
         log_file = os.path.join(ConfigLoader().get_path("output"), 'logs', 'ProcessExclusionsData.log')
         logger = config_loader.setup_task_logging('ProcessRegionBuffers', log_file)
@@ -51,22 +52,41 @@ class ProcessExclusionsData(luigi.Task):
         with open(project_settings_path, 'r') as file:
             project_settings = json.load(file)
         
-        place_name = project_settings["OSM_region_name"]
+        region_name = project_settings["OSM_region_name"]
+        place_name = project_settings["place_name_short"]
+        main_region = gpd.read_file(os.path.join(project_data_dir, "MAIN_REGION_POLYGON", f"{place_name}.shp"))
 
         ############## MAIN WORKFLOW #################
         # to ensure good logging, remember to pass logger=logger into whichever class you are using
         
         # load up the raw exclusion data
-        fp = os.path.join(raw_data_dir, f"{place_name}.osm.pbf")
+        fp = os.path.join(raw_data_dir, f"{region_name}.osm.pbf")
 
         # initialize the OSM object
         osm = pyrosm.OSM(fp)
 
         logger.info("OSM object loaded.")
-        boundaries = osm.get_boundaries(name="Aachen")
-        logger.info("Boundaries loaded.")
+        landuse = osm.get_landuse()
+        
+        residential = landuse[landuse["landuse"] == "residential"]
 
-        boundaries.plot(facecolor="none", edgecolor="blue", figsize=(10,6))
+        residential = residential.to_crs("EPSG:3035")
+        logger.info(f"residential data loaded. CRS: {residential.crs}")
+
+        logger.info("Clip residential data to the main region.")
+        #clipped_residential = gpd.clip(residential, main_region)
+        clipped_residential = residential
+        logger.info("Data clipped.")
+
+        # Check if the clipped GeoDataFrame is not empty
+        if not clipped_residential.empty:
+            # Save to a Shapefile
+            clipped_residential.to_file(os.path.join(processed_data_dir, "clipped_residential.shp"))
+            logger.info("Clipped clipped_residential data saved to file.")
+        else:
+            logger.error("No data after clipping with the main region.")
+
+        #landuse.plot(facecolor="none", edgecolor="blue", figsize=(10,6))
 
         # show the plot
         plt.show()
